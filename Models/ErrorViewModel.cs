ec1b7d0f81814c82dfd185ebada2ee0347a7ba44
Yamer <yamer@dept-info.crosemont.quebec>
2024-11-18 19:08:02 -0500

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFItness.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
