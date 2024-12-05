d00b7081da2cc0aaf17e92ae4ce741aa5909b762
Yamer <yamer@dept-info.crosemont.quebec>
2024-12-05 16:41:59 -0500

    Simplification du controlleur pour résoudre un bug

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,61 +1,31 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyFItness.Models;
-using System.Diagnostics;
 
-namespace MyFItness.Controllers
+namespace MyFItness.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Presentation()
-        {
-            return View();
-        }
-        public IActionResult EnterFitnessData()
-        {
-            return View();
-        }
-
-        public IActionResult Dashboard()
-        {
-            return View();
-        }
-
-        public IActionResult Activités()
-        {
-            return View();
-        }
-
-        public IActionResult Nutrition()
-        {
-            return View();
-        }
-
-        public IActionResult Information()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
