42f82c10125d9dc740f480d74a95ca6c297e68a5
Boudemagh Mohamed Jasser <2267413@crosemont.qc.ca>
2024-12-06 05:18:36 -0500

    Ajout de l'indexation dans le homecontroller ainsi que la gestion des informations de l'utilisateurs dans le acccount-controller et la page Information

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyFItness.Data;
+using MyFItness.Models;
+using System.Security.Claims;
+namespace MyFItness.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateInfos(ApplicationUser model)
+        {
+                var user = await _userManager.GetUserAsync(User);
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.DateOfBirth = model.DateOfBirth;
+                user.HeightCm = model.HeightCm;
+                user.WeightLbs = model.WeightLbs;
+                user.BodyFatPercentage = model.BodyFatPercentage;
+                user.Gender = model.Gender;
+                user.FitnessGoal = model.FitnessGoal;
+                user.IsTrainer = model.IsTrainer;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    
+                    return RedirectToAction("Index", "Home");
+                }
+
+            return View(model);
+            }
+
+        }
+
+
+    }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,17 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Information()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
